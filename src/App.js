914492836475f9d50df1fefa9844dbffa1916a31
adoumadje <adoumadjecaleb@gmail.com>
2022-06-22 17:58:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import ClassContextComponent from "./ClassContextComponent";
+import FunctionalContextComponent from "./FunctionalContextComponent";
+
+
+export const ThemeContext = React.createContext()
+
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState(true)
+
+  function toggleTheme() {
+    setDarkTheme(prevDarkTheme => !prevDarkTheme)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeContext.Provider value={darkTheme}>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+        <FunctionalContextComponent />
+        <ClassContextComponent />
+      </ThemeContext.Provider>
+    </>
   );
 }
 
---
+++ src/FunctionalContextComponent.jsx
@@ -0,0 +1,18 @@
+import React, {useContext} from 'react'
+import { ThemeContext } from './App'
+
+const FunctionalContextComponent = () => {
+    const darkTheme = useContext(ThemeContext)
+    const themeStyles = {
+        backgroundColor: darkTheme ? '#333' : '#ccc',
+        color: darkTheme ? '#ccc' : '#333',
+        padding: '2rem',
+        margin: '2rem'
+    }
+
+  return (
+    <div style={themeStyles}>Function Theme</div>
+  )
+}
+
+export default FunctionalContextComponent
